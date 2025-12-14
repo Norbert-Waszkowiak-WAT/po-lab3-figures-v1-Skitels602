788f6f63312479437ecec98754ec5c85bbabf80c
Skitels602 <12345tomtomtom@gmail.com>
2025-12-14 18:05:24 +0000

    67 skibidi

---
+++ point.h
@@ -0,0 +1,22 @@
+#ifndef PIONT
+#define PIONT
+#include<iostream>
+
+class Point {
+    private:
+        double x;
+        double y;
+    public:
+        Point(double x,double y);
+        Point(const Point & other);
+        bool equals(Point & other);
+        double const getX();
+        double const getY();
+        void flip();
+        void move(double x, double y);
+        std::string toString();
+
+        
+
+};
+#endif 
